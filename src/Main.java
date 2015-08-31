860f41d45a806b0189393ca888f1c561ce8e1fa2
dannl <JohnDannl@hotmail.com>
2015-08-31 16:51:44 +0800

    problem 1003 solved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
 
 public class Main {
-	public static void main(String[] args){
-		
+	public static void main(String args[]){
+		StreamTokenizer in=new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+		PrintWriter out=new PrintWriter(new OutputStreamWriter(System.out));
 	}
 }
